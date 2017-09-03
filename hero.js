bc305ce5d28b4705ec8e29a2f050c4ba803e2667
R.Murzakov <romuald.murzakov@gmail.com>
2017-09-03 20:27:02 +0100

    Tasks sortingmethods are added

--- hero.js
+++ hero.js
@@ -11,9 +11,27 @@ Hero.prototype.introduce = function(){
 
 Hero.prototype.eat =function(food){
   if (food.name == this.favouriteFood){
-  this.health += (food.replenishmentValue *1.5);}
+    this.health += (food.replenishmentValue *1.5);}
   else {this.health += food.replenishmentValue;}
   return this.health; 
+}
+
+Hero.prototype.sortTasksByLevel = function(){
+  return this.tasks.sort( function(a, b){
+    return a.level - b.level;
+  })
+}
+
+Hero.prototype.sortTasksByUrgency = function(){
+  return this.tasks.sort(function(a,b){
+    return a.urgency - b.urgency;
+  })
+}
+
+Hero.prototype.sortTasksByReward = function(){
+  return this.tasks.sort( function(a,b){
+    return b.reward - a.reward;
+  })
 }
 
 module.exports = Hero;
--- specs/game.js
+++ specs/game.js
@@ -7,9 +7,19 @@ describe("Heroes and Rats game", function(){
 
   beforeEach( function(){
     donatello = new Hero("Donatello");
-    save_world = new Task(1, 10, 50);
+    save_world = new Task(99, 1, 500);
+    eat_pizza = new Task(5, 5, 20 );
+    createNewTechnology = new Task(3, 10, 40);
+    sleep = new Task( 2, 2, 0);
+    workOut = new Task( 8, 6, 0);
     pizza = new Food("Pizza", 30);
     cheese = new Food("Cheese", 15);
+    donatello.tasks.push(save_world);
+    donatello.tasks.push(eat_pizza);
+    donatello.tasks.push(createNewTechnology);
+    donatello.tasks.push(sleep);
+    donatello.tasks.push(workOut);
+
 
   })
 
@@ -30,6 +40,22 @@ describe("Heroes and Rats game", function(){
   it("Favourite food gives 1,5 to health", function(){
     donatello.eat(pizza);
     assert.strictEqual(donatello.health,145);
+  })
+
+  it("Tasks Sort by level", function(){
+    donatello.sortTasksByLevel();
+    assert.deepEqual(donatello.tasks, [sleep, createNewTechnology, eat_pizza,workOut, save_world]);
+  })
+
+  it("Tasks sort by urgency", function(){
+    donatello.sortTasksByUrgency();
+    assert.deepEqual(donatello.tasks, [save_world, sleep, eat_pizza, workOut, createNewTechnology]);
+  })
+
+  it("Tasks sorty by reward", function(){
+    donatello.sortTasksByReward();
+    assert.deepEqual(donatello.tasks, [save_world, createNewTechnology, eat_pizza,sleep, workOut])
+
   })
 
 
